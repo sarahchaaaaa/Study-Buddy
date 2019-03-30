a71e61c3973860c2c376683c666ab7369c275608
Christina Youn <cyoun@nd.edu>
2019-03-30 17:40:16 -0400

    Christina code now

---
+++ currentCode/app/src/main/java/com/example/hp/studybuddy/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.hp.studybuddy;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_search:
+                    mTextMessage.setText("Click Create");
+                    return true;
+                case R.id.navigation_create:
+                    mTextMessage.setText("Create a Study Group");
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText("Click Create");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
