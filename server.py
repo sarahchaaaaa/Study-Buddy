e858e9419a1845fbf76a75228e77de558952d112
sarahchaaaaa <shwang5@nd.edu>
2019-03-30 11:48:06 -0400

    round 2

--- server.py
+++ server.py
@@ -4,10 +4,11 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 import sys
+import sqlite3
 
 # Constants
 
-PORT = 9991
+PORT = 9800
 
 # Handlers
 
@@ -33,37 +34,54 @@ class userHandler(tornado.web.RequestHandler):
 
 class netidHandler(tornado.web.RequestHandler):
 	def get(self):
-		name = self.get_arguments('NAME', None)
-		major = self.get_arguments('MAJOR', None)
+		name = self.get_arguments('NAME')
+		major = self.get_arguments('MAJOR')
 		response = {'NAME': name, 'NETID': netid, 'MAJOR': major}
 		self.write(response)
 #	def put(self):
 
 class locationsHandler(tornado.web.RequestHandler):
 	def get(self):
-		locations = self.get_arguments('NAME', None)
+		locations = self.get_arguments('NAME')
 		response = {'NAME': locations}
 		self.write(response)
 
 class locationNameHandler(tornado.web.RequestHandler):
 	def get(self):
-		name = self.get_arguments('NAME', None)
-		capacity = self.get_arguments('CAPACITY', None)
-		xCoord = self.get_arguments('XCOORDINATE', None)
-		yCoord = self.get_arguments('YCOORDINATE', None)
+		name = self.get_arguments('NAME')
+		capacity = self.get_arguments('CAPACITY')
+		xCoord = self.get_arguments('XCOORDINATE')
+		yCoord = self.get_arguments('YCOORDINATE')
 		response = {'NAME': name, 'CAPACITY': capacity, 'XCOORDINATE': xCoord, 'YCOORDINATE': yCoord}
 		self.write(response)
 class groupsHandler(tornado.web.RequestHandler):
 	def get(self):
-		gName = self.get_arguments('GID', None)
+		gName = self.get_arguments('GID')
 		response = {'GID': gName}
 		self.write(response) 
 # def put(self):
 class groupNameHandler(tornado.web.RequestHandler):
 	def get(self):
-		gName = self.get_arguments('NAME', None)
-#		creator = self.get_arguments('
-#		CREATOR CRN SIZE ABOUT'
+		gName = self.get_arguments('NAME')
+		creator = self.get_arguments('CREATOR')
+		crn = self.get_arguments('CRN')
+		size = self.get_arguments('SIZE')
+		about = self.get_arguments('ABOUT')
+		response = {'NAME': gName, 'CREATOR':creator, 'CRN':crn, 'SIZE':size, 'ABOUT': about}
+		self.write(response)
+
+class classHandler(tornado.web.RequestHandler):
+	def get(self):
+		classes = self.get_arguments('CLASS')
+		response = {'CLASS':classes}
+		self.write(response)
+
+class crnHandler(tornado.web.RequestHandler):
+	def get(self):
+		classes = self.get_arguments('CLASS')
+		crn = self.get_arguments('CRN')
+		response = {'CLASS':classes, 'CRN': crn}
+		self.write(response)
 
 #Application		
 
@@ -80,6 +98,6 @@ Application = tornado.web.Application([
 Application.listen(PORT)
 
 # Main Execution
-PORT = sys.argv[1]
+#PORT = sys.argv[1]
 tornado.options.parse_command_line()
 tornado.ioloop.IOLoop.current().start()
